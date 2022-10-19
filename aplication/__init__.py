2e34e6554708850e265ae699ce6f9fa6197b8a03
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-18 19:56:47 -0500

    Cree el formulario para registrar un usuario y formule la función para obtener la informaicón

--- aplication/__init__.py
+++ aplication/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -15,6 +15,13 @@ db = SQLAlchemy(app)
 
 db.create_all()
 
-@app.route('/')
+@app.route('/',methods=['POST'])
 def index():
+    nombre=request.form['nombre']
+    email = request.form['email']
+    ciudad=request.form['ciudad']
+    print(nombre)
+    print(email)
+    print(ciudad)
+
     return render_template('index.html')
