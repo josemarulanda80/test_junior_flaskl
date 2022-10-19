d57763a39bfc40636026765bcee7f0d82d054409
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-19 13:47:45 -0500

    se registra las creaciones en un txt

--- aplication/__init__.py
+++ aplication/__init__.py
@@ -5,8 +5,8 @@ from flask import Flask,render_template,request, session,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-
+# current date and time
+curDT = datetime.now()
 
 app = Flask(__name__)
 
@@ -36,11 +36,18 @@ def registrar():
     nombre=request.form['nombre']
     email = request.form['email']
     ciudad=request.form['ciudad']
+
+    user = User(nombre=nombre,email=email,ciudad=ciudad)
+
+    date_time = curDT.strftime("%m/%d/%Y, %H:%M:%S")
+    cadena = f"El nombre es {nombre} con correo electronico {email} de la ciudad {ciudad} en la fecha {date_time}"
+    f = open('log.txt','a')
+    f.write('\n' + cadena)
+    f.close()
     f = open ('./aplication/log.txt','r')
     mensaje = f.read()
     print(mensaje)
     f.close()
-    user = User(nombre=nombre,email=email,ciudad=ciudad)
     db.session.add(user)
     db.session.commit()
     return redirect('/')
