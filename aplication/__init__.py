010e42f81df65793a6aa9fc0a2d56ca009126c82
josemarulanda80 <josemarulanda63@hotmail.com>
2022-10-19 10:52:11 -0500

    cree la función de mostrar registros ademas agregue txt para escribir los registros

--- aplication/__init__.py
+++ aplication/__init__.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 
 from flask import Flask,render_template,request, session,redirect
 from flask_sqlalchemy import SQLAlchemy
-
+from datetime import datetime
 
 
 
@@ -36,11 +36,19 @@ def registrar():
     nombre=request.form['nombre']
     email = request.form['email']
     ciudad=request.form['ciudad']
+    f = open ('./aplication/log.txt','r')
+    mensaje = f.read()
+    print(mensaje)
+    f.close()
     user = User(nombre=nombre,email=email,ciudad=ciudad)
     db.session.add(user)
     db.session.commit()
     return redirect('/')
 
-
+@app.route('/all')
+def all():
+    users=User.query.all()
+    print(users)
+    return render_template('show.html',users=users)
 #Esto siempre va abajo  del todo
 db.create_all()
